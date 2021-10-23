a15838adc709b1b47e72f5562acec42b99c71052
TonySaar <sarapinad@gmail.com>
2021-10-23 19:27:56 +0300

    add 2nd part of homework_01

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,19 +2,20 @@
 Домашнее задание №1
 Функции и структуры данных
 """
+from math import sqrt
 
 
-def power_numbers(*list):
-    res= []
-    for elem in list:
+def power_numbers(*numbers):
+    res = []
+    for elem in numbers:
         res.append(elem * elem)
     return res
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
+    # """
+    # функция, которая принимает N целых чисел,
+    # и возвращает список квадратов этих чисел
+    # >>> power_numbers(1, 2, 5, 7)
+    # <<< [1, 4, 25, 49]
+    # """
 
 
 # filter types
@@ -22,25 +23,56 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
-def is_prime(num):
-    d = 2
-    while num % d != 0:
-        d += 1
-    return d == num
 
-def filter_numbers(numbers,type):
-    for elem in numbers[:]:
-        if type == EVEN:
-                if elem % 2 != 0:
-                numbers.remove(elem)
-        return numbers
-#     """
-#     функция, которая на вход принимает список из целых чисел,
-#     и возвращает только чётные/нечётные/простые числа
-#     (выбор производится передачей дополнительного аргумента)
-#
-#     >>> filter_numbers([1, 2, 3], ODD)
-#     <<< [1, 3]
-#     >>> filter_numbers([2, 3, 4, 5], EVEN)
-#     <<< [2, 4]
-#     """
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    limit = sqrt(n)
+    i = 2
+    while i <= limit:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def is_odd(n):
+    if n % 2 != 0:
+        return True
+    else:
+        return False
+
+
+def filter_numbers(num, filter_type):
+    if filter_type == EVEN:
+        num = list(filter(is_even, num))
+        return num
+    if filter_type == ODD:
+        num = list(filter(is_odd, num))
+        return num
+    if filter_type == PRIME:
+        num = list(filter(is_prime, num))
+        return num
+
+
+print(filter_numbers([2, 3, 4, 5, 6, 7, 8, 9, 10], ODD))
+
+    # """
+    # функция, которая на вход принимает список из целых чисел,
+    # и возвращает только чётные/нечётные/простые числа
+    # (выбор производится передачей дополнительного аргумента)
+    #
+    # >>> filter_numbers([1, 2, 3], ODD)
+    # <<< [1, 3]
+    # >>> filter_numbers([2, 3, 4, 5], EVEN)
+    # <<< [2, 4]
+    # """
